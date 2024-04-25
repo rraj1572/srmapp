cd5480a74e9993e5664deb3232c3b092bd33cb83
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2024-04-25 16:20:54 +0530

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,11 @@
+<?php
+// Reset visit count
+$visitFile = 'visit_count.txt';
+file_put_contents($visitFile, 0);
+
+// Reset download count
+$downloadFile = 'download_count.txt';
+file_put_contents($downloadFile, 0);
+
+echo 'Counts have been reset successfully.';
+?>
