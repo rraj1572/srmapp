1d521636779da8b73e6f4d0472f38a466209b5b3
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2024-04-25 17:11:48 +0530

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,32 +1,38 @@
 <?php
+// Define the reset function
+function resetCounts() {
+    $visitFile = 'visit_count.txt';
+    file_put_contents($visitFile, 0);
+
+    $downloadFile = 'download_count.txt';
+    file_put_contents($downloadFile, 0);
+}
+
 // Function to get download count
 function getDownloadCount() {
     $downloadFile = 'download_count.txt';
-    return (int)file_get_contents($downloadFile);
+    return file_get_contents($downloadFile);
 }
 
 // Function to get visit count
 function getVisitCount() {
     $visitFile = 'visit_count.txt';
-    return (int)file_get_contents($visitFile);
+    return file_get_contents($visitFile);
 }
 
-if (isset($_GET['reset'])) {
-$visitFile = 'visit_count.txt';
-file_put_contents($visitFile, 0);
-
-$downloadFile = 'download_count.txt';
-file_put_contents($downloadFile, 0);
+// Check if form is submitted for resetting counts
+if (isset($_POST['download'])) {
+    resetCounts(); // Reset counts
+    // Redirect back to the current page to avoid form resubmission
+    header("Location: {$_SERVER['REQUEST_URI']}");
+    exit();
 }
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link rel="icon" href="ic_icon.png">
-
+    <link rel="icon" href="ic_icon.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Statistics</title>
@@ -68,22 +74,20 @@ file_put_contents($downloadFile, 0);
         button:hover {
             background-color: #0056b3;
         }
-.center {
+        .center {
             display: block;
             margin-left: auto;
             margin-right: auto;
-}
-
+        }
     </style>
 </head>
 <body>
     <div class="container">
-<img src="ic_icon.png" widhth='200px' height = '200px' class="center"></img>
-
+        <img src="ic_icon.png" width="200px" height="200px" class="center">
         <h1>Statistics</h1>
         <p>Number of downloads: <?php echo getDownloadCount(); ?></p>
         <p>Number of visits: <?php echo getVisitCount(); ?></p>
-        <form method="post" action="reset.php">
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <button type="submit" name="download">Reset Counts</button>
         </form>
     </div>
